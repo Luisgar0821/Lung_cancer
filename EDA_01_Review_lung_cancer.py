035de5932741ff143af656db0762abac6daf1650
Luis Gar <luisangelgarcia0821@gmail.com>
2025-03-02 09:58:38 -0500

    add data_cleaning

--- EDA_01_Review_lung_cancer.py
+++ EDA_01_Review_lung_cancer.py
@@ -1,5 +1,3 @@
-##-----------------------------------------------------------------------
-##libraries
 import pandas as pd
 
 ##-----------------------------------------------------------------------
@@ -31,4 +29,13 @@ print(df.isnull().sum())
 # valores únicos de algunas columnas clave
 print("\nValores únicos en la columna 'Gender':", df['Gender'].unique())
 print("Valores únicos en la columna 'Smoker':", df['Smoker'].unique())
+print("Valores únicos en la columna 'Passive_Smoker':", df['Passive_Smoker'].unique())
+print("Valores únicos en la columna 'Family_History':", df['Family_History'].unique())
 print("Valores únicos en la columna 'Lung_Cancer_Diagnosis':", df['Lung_Cancer_Diagnosis'].unique())
+print("Valores únicos en la columna 'Cancer_Stage':", df['Cancer_Stage'].unique())
+print("Valores únicos en la columna 'Adenocarcinoma_Type':", df['Adenocarcinoma_Type'].unique())
+print("Valores únicos en la columna 'Air_pollution_exposure':", df['Air_Pollution_Exposure'].unique())
+print("Valores únicos en la columna 'Lung_Cancer_Diagnosis':", df['Lung_Cancer_Diagnosis'].unique())
+print("Valores únicos en la columna 'Developed_or_Developing':", df['Developed_or_Developing'].unique())
+
+
